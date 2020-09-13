83e563e91c75b48c8a8a59e02e1f3b94d43352e5
gissellevillanueva <70177957+gissellevillanueva@users.noreply.github.com>
2020-09-13 10:18:04 -0400

    saver

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ age = "17";
 alert("My name is " + firstName + lastName);
 alert("I am" + age + " years old");
 
+let gisselleVillanueva
+
+if (age > 21) {
+    gisselleVillanueva = " am legally able";
+} else if (age >= 21) {
+    gisselleVillanueva = " am just barely able";
+} else {
+    gisselleVillanueva = "t is illegal for me"
+}
+
+alert("I" + gisselleVillanueva + " to drink in the states.")
+
+console.log("Although I can struggle to understand concepts, I still have a lot of fun learning how to code :)")
+
 
