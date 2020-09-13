79695b3605ded894da23db64217ef863cab416eb
gissellevillanueva <70177957+gissellevillanueva@users.noreply.github.com>
2020-09-12 21:53:08 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var firstName;
+firstName = "Gisselle";
+
+var lastName;
+lastName = " Villanueva";
+
+var age;
+age = "17";
+
+alert("My name is " + firstName + lastName);
+alert("I am" + age + " years old");
+
+
